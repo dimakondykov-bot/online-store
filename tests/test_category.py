b30e575b98036499d396ccf2b673a57c5bf6c8ad
Dmitry <dimakondykov@gmail.com>
2026-02-05 23:45:50 +0300

    доработка fix 2

--- tests/test_category.py
+++ tests/test_category.py
@@ -7,13 +7,13 @@ def test_category_creation(sample_category: Category):
     assert sample_category.name == "Electronics"
     assert len(sample_category.get_products_list()) == 5
     assert Category.total_categories == 2
-    assert Category.total_products == 5
+    assert Category.total_product == 5
 
 def test_category_with_empty_products():
 
     category = Category("Books", "Various books")
     assert category.products_count == 0
-    assert Category.total_products == 0
+    assert Category.total_product == 0
 
 def test_category_add_product(sample_category: Category, sample_product: Product):
     initial_count = len(sample_category.get_products_list())
